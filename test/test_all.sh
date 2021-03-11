ab68e37ea10669c095194e7899fb6367809e930c
kdstevens <kdstevens@DESKTOP-9QDSDNR.localdomain>
2021-03-10 23:46:06 -0500

    Updated test_all.sh

--- test/test_all.sh
+++ test/test_all.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 echo "Running test 1..."
-timeout 20s bash test1.sh
+timeout 20s bash runtest.sh test1config.yml
 
 echo "Running test 2..."
-timeout 20s bash test2.sh
+timeout 20s bash runtest.sh test2config.yml
 
 echo "Running test 3..."
-timeout 20s bash test3.sh
+timeout 20s bash runtest.sh test3config.yml
 
 echo "Running test 4..."
-timeout 20s bash test4.sh
+timeout 20s bash runtest.sh test4config.yml
+
+echo "Running test 5..."
+timeout 20s bash runtest.sh test5config.yml
+
+echo "Running test 6..."
+timeout 20s bash runtest.sh test6config.yml
 
 bash test_cleanup.sh
