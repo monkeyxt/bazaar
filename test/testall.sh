0dd8a33cb04519319fc4f52b77f909454fc7b54a
kdstevens <kdstevens@umass.edu>
2021-03-17 21:30:54 -0400

    Recreated testall.sh

---
+++ test/testall.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Running all test scripts for 30 seconds each..."
+
+echo "Running test 1..."
+timeout 30s bash runtest.sh test1config.yml
+
+echo "Running test 2..."
+timeout 30s bash runtest.sh test2config.yml
+
+echo "Running test 3..."
+timeout 30s bash runtest.sh test3config.yml
+
+echo "Running test 4..."
+timeout 30s bash runtest.sh test4config.yml
+
+echo "Running test 5..."
+timeout 30s bash runtest.sh test5config.yml
+
+echo "Running test 6..."
+timeout 30s bash runtest.sh test6config.yml
+
+echo "Running test 7..."
+timeout 30s bash runtest.sh test7config.yml
+
+echo "Running test 8..."
+timeout 30s bash runtest.sh test8config.yml
+
+echo "Running test 9..."
+timeout 30s bash runtest.sh test9config.yml
+
+echo "Running test 10..."
+timeout 30s bash runtest.sh test10config.yml
